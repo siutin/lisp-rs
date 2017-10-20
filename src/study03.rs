e470c4387320da708a5c40dde4c466040e904519
osiutino <osiutino@gmail.com>
2017-10-20 23:05:03 +0800

    study03 - update string

--- src/study03.rs
+++ src/study03.rs
@@ -3,18 +3,17 @@ use std::cell::RefCell;
 
 #[derive(Debug)]
 struct Container<'a> {
-    i: &'a RefCell<HashMap<String, i32>>
+    i: &'a RefCell<HashMap<&'a str, i32>>
 }
 
 fn recursive(mut c: Container) {
     println!("recursive");
     println!("container = {:?}", c);
-    match c.i.borrow().get(&"default".to_string()) {
+    match c.i.borrow().get(&"default") {
         Some(&i) => {
             println!("i = {}", i);
             if i > 0 {
-//                *c.i.borrow_mut() -= 1;
-                c.i.borrow_mut().insert("default".to_string(), i - 1);
+                c.i.borrow_mut().insert("default", i - 1);
                 recursive(c);
             }
         },
@@ -27,7 +26,7 @@ fn main () {
     println!("Hello World");
 
     let x = RefCell::new(HashMap::new());
-    x.borrow_mut().insert("default".to_string(), 10);
+    x.borrow_mut().insert("default", 10);
     let c = Container {
         i : &x
     };
