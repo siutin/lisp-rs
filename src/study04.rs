3556f31c750afce52ac66ef5a5b2023d9380a63e
osiutino <osiutino@gmail.com>
2017-10-21 14:01:05 +0800

    study 04 rest experiment
    (cherry picked from commit b5a6d09)

--- src/study04.rs
+++ src/study04.rs
@@ -1,6 +1,7 @@
 macro_rules! tuplet {
  { ($y:ident $(, $x:ident)*) = $v:expr } => {
     let ($y,$($x),*) = tuplet!($v ; 1 ; ($($x),*) ; ($v.get(0)) ); };
+
  { ($y:ident , * $x:ident) = $v:expr } => {
     let ($y, $x) = {
         if let Some((x,y)) = $v.split_first() {
@@ -14,18 +15,28 @@ macro_rules! tuplet {
         }
     };
 };
+
+ { ($y:ident $(, $x:ident)* , * $z:ident) = $v:expr } => {
+    let ($y, $($x),*) = tuplet!($v ; 1 ; ($($x),*) ; ($v.get(0)) );
+    let $z = {
+       println!("{:?}",&$v[2..]);
+       tuplet!((xy,*z)= &$v[2..]);
+       z
+    };
+};
+
  { $v:expr ; $j:expr ; ($y:ident $(, $x:ident)*) ; ($($a:expr),*) } => {
     tuplet!( $v ; $j+1 ; ($($x),*) ; ($($a),*,$v.get($j)) ) };
  { $v:expr ; $j:expr ; () ; $accu:expr } => { $accu }
 }
 
 
-fn main () {
-    let v = vec![1, 2, 3, 4, 5];
-    tuplet!((a,*b) = v);
+fn main() {
+    let v = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
+    tuplet!((a,b,*c) = v);
 
     println!("a = {:?}", a);
     println!("b = {:?}", b);
-    //    println!("c = {:?}", c);
+    println!("c = {:?}", c);
     //    println!("d = {:?}", d);
 }
