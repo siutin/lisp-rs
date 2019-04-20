6918d4b50ba86635e0a4f9ac58a8c8814c9eedf0
osiutino <osiutino@gmail.com>
2019-04-20 14:52:24 +0800

    extract InPort struct

--- src/study.rs
+++ src/study.rs
@@ -3,36 +3,53 @@ extern crate regex;
 use regex::Regex;
 use std::result::Result;
 fn main () {
-//	let text = r#"(begin (print "hello world"))"#;
 	let text = r#"(begin (+ 1 2 (+ 3 4 5) 6))"#;
 	println!("text = {:?}", text);
 
-	let result = in_port(text);
-	println!("result: {:?}", result)
+	let mut o = InPort::new(text);
+	match o.next_token() {
+		Ok(Some(s)) => println!("next token = {:?}", s),
+		_ => 	println!("no more next token")
+	}
 }
 
-fn in_port(text: &str) -> Result<String, &'static str> {
+struct InPort {
+	token: Option<String>,
+	line: String
+}
 
-	let re = Regex::new(r#"\s*(,@|[('`,)]|"(?:[\\].|[^\\"])*"|;.*|[^\s('"`,;)]*)(.*)"#).unwrap();
-
-	let mut line = String::from(text);
-
-	if line.is_empty() {
-		return Err(&"empty line")
-	}
-
-	while !line.is_empty() {
-		let t = line.clone();
-		let caps_option = re.captures(&t);
-		match caps_option {
-			Some(caps) => {
-				let token = caps.get(1).map_or("", |m| m.as_str());
-				line = String::from(caps.get(2).map_or("", |m| m.as_str()));
-				println!(" 1 = {}", token);
-			},
-			None => {}
+impl InPort {
+	fn new (text: &str) -> InPort {
+		InPort {
+			token: None,
+			line: String::from(text)
 		}
 	}
+	fn token(&self) -> Option<String> {
+		self.token.clone()
+	}
 
-	Ok("GOOD".to_string())
+	fn next_token(&mut self) -> Result<Option<String>, &'static str> {
+
+		let re = Regex::new(r#"\s*(,@|[('`,)]|"(?:[\\].|[^\\"])*"|;.*|[^\s('"`,;)]*)(.*)"#).unwrap();
+
+		if self.line.is_empty() {
+			return Err(&"empty line")
+		}
+
+		while !self.line.is_empty() {
+			let t = self.line.clone();
+			let caps_option = re.captures(&t);
+			match caps_option {
+				Some(caps) => {
+					let token = caps.get(1).map_or("", |m| m.as_str());
+					self.line = String::from(caps.get(2).map_or("", |m| m.as_str()));
+					self.token = Some(token.to_string());
+					return Ok(self.token.clone())
+				},
+				None => {}
+			}
+		}
+		Ok(None)
+	}
 }
